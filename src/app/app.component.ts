c4b9c473dc80638fd83fb51d8aa2294c6263e9eb
Moca9801 <aimc9801@gmail.com>
2023-05-27 14:16:23 -0600

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calculator';
+export class AppComponent implements OnInit{
+
+  pantalla: string;
+
+  constructor(){
+    this.pantalla = '0';
+  }
+
+  ngOnInit(): void {
+    console.log('Aplicación de calculadora cargada')
+  }
+
+  handleClick(value: string){
+    if(this.pantalla === '0' || this.pantalla == 'Error!'){
+      this.pantalla = value;
+    }else{
+      this.pantalla += value;
+    }
+  }
+
+  delete(){
+    if(this.pantalla != '0' ){
+      this.pantalla = this.pantalla.slice(0, -1);
+    }
+
+    if(this.pantalla === '' || this.pantalla == 'Error!'){
+      this.pantalla = '0';
+    }
+    
+  }
+
+  solution(){
+    try{
+      this.pantalla = eval(this.pantalla);
+    }catch{
+      this.pantalla = "Error!"
+    }
+  }
+
+  clear(){
+    this.pantalla = '0';
+  }
 }
+
